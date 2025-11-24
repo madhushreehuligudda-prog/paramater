5aabf3990b0f4d169874ece9dcee2ac416f245bd
madhushreehulligudda <madhushreehuligudda@gmail.com>
2025-11-24 16:26:38 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    
+    x = input("Enter first number: ")
+    y = input("Enter second number: ")
+
+    print("sum:", add(int(x), int(y)))
