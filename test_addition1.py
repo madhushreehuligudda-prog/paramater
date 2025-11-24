cfc3a9daa2f94ed635f5889d7fb262453906899c
madhushreehulligudda <madhushreehuligudda@gmail.com>
2025-11-24 16:57:02 +0530

    Update test_addition1.py

--- test_addition1.py
+++ test_addition1.py
@@ -1,12 +1,8 @@
-def add(a, b):
-    return a + b
+from addition import add
 
-# Tests
 def test_add_positive_numbers():
-    assert add(int("2"), int("3")) == 5
-    
+    assert add(2, 3) == 5
 def test_add_negative_numbers():
-    assert add(int("-4"), int("-6")) == -10
-    
+    assert add(-4, -6) == -10
 def test_add_zero():
-    assert add(int("0"), int("5")) == 5
+    assert add(0, 5) == 5
